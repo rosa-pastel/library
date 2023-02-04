3f43deaa548b7daab56e3af85082c0c0bf56c5b3
rosa-pastel <zeynepranamutlu@outlook.com>
2023-02-04 06:26:50 +0300

    Refactor code

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-const library = [];
-
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.toggleRead = function () {
-    if (this.read) {
-      this.read = false;
-    } else {
-      this.read = true;
-    }
-    console.log(this);
+  this.toggleRead = () => {
+    this.read = !this.read;
   };
 }
 
@@ -19,62 +12,55 @@ function removeBook() {
   this.parentNode.remove();
 }
 
-function displayBook(book) {
-  const bookCards = document.querySelector("div.books");
+function addCardElement(label, content) {
+  const labelDiv = document.createElement("div");
+  labelDiv.classList.add(label.toLowerCase());
+  labelDiv.textContent = label;
+  const contentElement = document.createElement("p");
+  contentElement.appendChild(document.createTextNode(content));
+  labelDiv.appendChild(contentElement);
+  return labelDiv;
+}
 
-  const bookCard = document.createElement("div");
-
-  const title = document.createElement("div");
-  title.classList.add("title");
-  title.textContent = "Title";
-  const titleContent = document.createElement("p");
-  titleContent.appendChild(document.createTextNode(book.title));
-  title.appendChild(titleContent);
-
-  bookCard.appendChild(title);
-
-  if (book.author) {
-    const author = document.createElement("div");
-    author.classList.add("author");
-    author.textContent = "Author";
-    const authorContent = document.createElement("p");
-    authorContent.appendChild(document.createTextNode(book.author));
-    author.appendChild(authorContent);
-
-    bookCard.appendChild(author);
-  }
-
-  if (book.pages) {
-    const pages = document.createElement("div");
-    pages.classList.add("pages");
-    pages.textContent = "Pages";
-    const pagesContent = document.createElement("p");
-    pagesContent.appendChild(document.createTextNode(book.pages));
-    pages.appendChild(pagesContent);
-
-    bookCard.appendChild(pages);
-  }
-
-  const read = document.createElement("div");
-  read.classList.add("read");
-  read.textContent = "Read";
+function addReadStatus(book) {
+  const readDiv = document.createElement("div");
+  readDiv.classList.add("read");
+  readDiv.textContent = "Read";
   const readBox = document.createElement("input");
   readBox.type = "checkbox";
   readBox.checked = book.read;
   readBox.classList.add("read");
-  const toggleFunc = book.toggleRead.bind(book);
-  readBox.addEventListener("change", toggleFunc);
-  read.appendChild(readBox);
-  bookCard.appendChild(read);
+  readBox.addEventListener("change", book.toggleRead.bind(book));
+  readDiv.appendChild(readBox);
+  return readDiv;
+}
 
+function addRemoveButton() {
   const removeButton = document.createElement("button");
   removeButton.classList.add("remove");
   removeButton.addEventListener("click", removeBook);
   removeButton.appendChild(document.createTextNode("Remove"));
-  bookCard.appendChild(removeButton);
+  return removeButton;
+}
 
+function createBookCard(book) {
+  const bookCard = document.createElement("div");
+  bookCard.appendChild(addCardElement("Title", book.title));
+  if (book.author) {
+    bookCard.appendChild(addCardElement("Author", book.author));
+  }
+  if (book.pages) {
+    bookCard.appendChild(addCardElement("Pages", book.pages));
+  }
+  bookCard.appendChild(addReadStatus(book));
+  bookCard.appendChild(addRemoveButton(book));
   bookCard.classList.add("card");
-  bookCards.appendChild(bookCard);
+  return bookCard;
+}
+
+function displayBook(book) {
+  const books = document.querySelector("div.books");
+  books.appendChild(createBookCard(book));
 }
 
 function addBookToLibrary(event) {
@@ -89,19 +75,25 @@ function addBookToLibrary(event) {
     const pages = document.querySelector("input#pages").value;
     const read = document.querySelector("input#read").checked;
     const book = new Book(title, author, pages, read);
-    library.push(book);
     displayBook(book);
   }
 }
-const form = document.querySelector(".form-container");
-const closeForm = document.querySelector(".close-form");
-const newBookButton = document.querySelector(".new");
-newBookButton.addEventListener("click", () => {
-  form.style.display = "block";
-});
-closeForm.addEventListener("click", () => {
-  form.style.display = "none";
-});
 
-const addBookButton = document.querySelector("button.submit");
-addBookButton.addEventListener("click", addBookToLibrary);
+function addEventListeners() {
+  const form = document.querySelector(".form-container");
+
+  const newBookButton = document.querySelector(".new");
+  newBookButton.addEventListener("click", () => {
+    form.style.display = "block";
+  });
+
+  const closeForm = document.querySelector(".close-form");
+  closeForm.addEventListener("click", () => {
+    form.style.display = "none";
+  });
+
+  const addBookButton = document.querySelector("button.submit");
+  addBookButton.addEventListener("click", addBookToLibrary);
+}
+
+addEventListeners();
