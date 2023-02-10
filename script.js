8c9bb6d1088036377835cbee11355ffd18a8c8a4
rosa-pastel <zeynepranamutlu@outlook.com>
2023-02-10 16:00:04 +0300

    Implement class structure

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.toggleRead = () => {
-    this.read = !this.read;
-  };
+function addRemoveButton() {
+  const removeButton = document.createElement("button");
+  removeButton.classList.add("remove");
+  removeButton.addEventListener("click", () =>
+    removeButton.parentNode.remove()
+  );
+  removeButton.appendChild(document.createTextNode("Remove"));
+  return removeButton;
 }
 
-function removeBook() {
-  this.parentNode.remove();
-}
-
-function addCardElement(label, content) {
+function createCardElement(label, content) {
   const labelDiv = document.createElement("div");
-  labelDiv.classList.add(label.toLowerCase());
+  labelDiv.classList.add("card-label");
   labelDiv.textContent = label;
   const contentElement = document.createElement("p");
   contentElement.appendChild(document.createTextNode(content));
@@ -22,45 +18,46 @@ function addCardElement(label, content) {
   return labelDiv;
 }
 
-function addReadStatus(book) {
-  const readDiv = document.createElement("div");
-  readDiv.classList.add("read");
-  readDiv.textContent = "Read";
-  const readBox = document.createElement("input");
-  readBox.type = "checkbox";
-  readBox.checked = book.read;
-  readBox.classList.add("read");
-  readBox.addEventListener("change", book.toggleRead.bind(book));
-  readDiv.appendChild(readBox);
-  return readDiv;
-}
-
-function addRemoveButton() {
-  const removeButton = document.createElement("button");
-  removeButton.classList.add("remove");
-  removeButton.addEventListener("click", removeBook);
-  removeButton.appendChild(document.createTextNode("Remove"));
-  return removeButton;
-}
-
-function createBookCard(book) {
-  const bookCard = document.createElement("div");
-  bookCard.appendChild(addCardElement("Title", book.title));
-  if (book.author) {
-    bookCard.appendChild(addCardElement("Author", book.author));
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.toggleRead = () => {
+      this.read = !this.read;
+    };
   }
-  if (book.pages) {
-    bookCard.appendChild(addCardElement("Pages", book.pages));
-  }
-  bookCard.appendChild(addReadStatus(book));
-  bookCard.appendChild(addRemoveButton(book));
-  bookCard.classList.add("card");
-  return bookCard;
-}
 
-function displayBook(book) {
-  const books = document.querySelector("div.books");
-  books.appendChild(createBookCard(book));
+  createReadStatus() {
+    const readDiv = document.createElement("div");
+    readDiv.classList.add("read");
+    readDiv.textContent = "Read";
+    const readBox = document.createElement("input");
+    readBox.type = "checkbox";
+    readBox.checked = this.read;
+    readBox.addEventListener("change", this.toggleRead());
+    readDiv.appendChild(readBox);
+    return readDiv;
+  }
+
+  createBookCard() {
+    const bookCard = document.createElement("div");
+    bookCard.appendChild(createCardElement("Title", this.title));
+    if (this.author)
+      bookCard.appendChild(createCardElement("Author", this.author));
+    if (this.pages)
+      bookCard.appendChild(createCardElement("Pages", this.pages));
+    bookCard.appendChild(this.createReadStatus());
+    bookCard.appendChild(addRemoveButton());
+    bookCard.classList.add("card");
+    return bookCard;
+  }
+
+  displayBook() {
+    const books = document.querySelector("div.books");
+    books.appendChild(this.createBookCard());
+  }
 }
 
 function addBookToLibrary(event) {
@@ -75,7 +72,7 @@ function addBookToLibrary(event) {
     const pages = document.querySelector("input#pages").value;
     const read = document.querySelector("input#read").checked;
     const book = new Book(title, author, pages, read);
-    displayBook(book);
+    book.displayBook();
   }
 }
 
