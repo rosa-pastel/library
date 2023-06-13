3c53d7032d0a70f4c2a36b67cdb827f06ec45ce0
rosa-pastel <zeynepranamutlu@outlook.com>
2023-06-13 21:08:20 +0300

    Initialize firebase

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBtW9OkALtjcy7JmEOW0Kk_2CYr4wj0p8k",
+  authDomain: "library-app-21.firebaseapp.com",
+  projectId: "library-app-21",
+  storageBucket: "library-app-21.appspot.com",
+  messagingSenderId: "1002177492296",
+  appId: "1:1002177492296:web:b814af4b761a9225392f7d",
+  measurementId: "G-ME6YGLZPVJ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function addRemoveButton() {
   const removeButton = document.createElement("button");
   removeButton.classList.add("remove");
